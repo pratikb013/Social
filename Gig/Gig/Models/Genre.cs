af6fe956ff5d79187f4af6751f52f2459970a899
pratik <pratik>
2016-11-09 22:39:48 -0500

    OverrideConventionToGig

--- Gig/Gig/Models/Genre.cs
+++ Gig/Gig/Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace Gig.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gig.Models
 {
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
