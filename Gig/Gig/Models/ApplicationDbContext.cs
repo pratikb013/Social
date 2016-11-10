e6e30ebacf51f07d4e8ebd7f03d9b6a3456c333b
pratik <pratik>
2016-11-09 22:20:37 -0500

    GigTabel

--- Gig/Gig/Models/ApplicationDbContext.cs
+++ Gig/Gig/Models/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace Gig.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ Gig/Gig/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace Gig.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
